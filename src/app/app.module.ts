91c8ec9d10cba82a49e31ef41219f68453a66fcf
Unknown <marivint.yvann@gmail.com>
2019-03-05 15:50:56 +0100

    correctif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
