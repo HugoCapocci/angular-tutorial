d470d6857369597aaac17ef985798fb904b607d2
Unknown <marivint.yvann@gmail.com>
2019-03-05 15:33:16 +0100

    ajout formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimelineComponent
+    TimelineComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -10,12 +10,14 @@ export class TimelineComponent implements OnInit {
      {
       created_at: 'Wed Apr 05 12:30:12 +0000 2017',
       id: 1,
+      like: 0,
       text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
       user: 'James Braddock'
     },
     {
       created_at: 'Thu Apr 06 15:24:15 +0000 2017',
       id: 2,
+      like: 2,
       text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
       user: 'Bob kelso'
     },
@@ -24,6 +26,22 @@ export class TimelineComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave(value){
+  	this.tweets.push(
+  		{	
+	      created_at: new Date().toDateString(),
+	      id: this.tweets.length + 1,
+	      like: 0,
+	      text: value,
+	      user: 'Yvann'
+  		}
+  	)
+  }  
+
+  likeTweet(tweet){
+  	tweet.like = tweet.like + 1;
   }
 
 }
