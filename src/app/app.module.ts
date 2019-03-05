2e2211f7eb2fc5e2d462a83bf725c1cbc04c1816
Krusherr <32904340+Krusherr@users.noreply.github.com>
2019-03-05 11:33:06 +0100

    Twiiter with Date Ago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimelineComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -11,13 +11,15 @@ export class TimelineComponent implements OnInit {
       created_at: 'Wed Apr 05 12:30:12 +0000 2017',
       id: 1,
       text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
-      user: 'James Braddock'
+      user: 'James Braddock',
+      like: 31, 
     },
     {
       created_at: 'Thu Apr 06 15:24:15 +0000 2017',
       id: 2,
       text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
-      user: 'Bob kelso'
+      user: 'Bob kelso',
+      like: 2, 
     },
   ];
 
