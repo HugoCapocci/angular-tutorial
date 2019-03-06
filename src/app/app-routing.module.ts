3cd0622a82fb1ce45d854a20a3656638c5e125ed
HugoCapocci <h.capocci@gmail.com>
2019-03-06 23:01:39 +0100

    Add routing and default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TimelineComponent } from './timeline/timeline.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/timeline', pathMatch: 'full' },
+  { path: 'timeline', component: TimelineComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
