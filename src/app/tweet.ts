b300adbb961e0cdbcd13046dc77a7d0fb8791f65
hcapocci <hcapoccci@talentsoft.com>
2019-03-06 18:16:30 +0100

    extract user-box, add communication from child to parent

--- src/app/tweet.ts
+++ src/app/tweet.ts
@@ -1,8 +1,8 @@
 export interface Tweet {
-  created_at: string,
-  id: number,
-  text: string,
-  user: string,
-  favoriteCount?: number,
-  isLiked?: boolean
+  created_at: string;
+  id: number;
+  text: string;
+  user: string;
+  favoriteCount?: number;
+  isLiked?: boolean;
 }
